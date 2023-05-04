1529555faff0d254e814b802c97076813f564077
Pedro Sudré <pedrosudre04@gmail.com>
2023-05-04 17:16:10 -0300

    tratamento de erros

---
+++ src/main/java/com/example/backendapi/controller/FilmeController.java
@@ -0,0 +1,34 @@
+package com.example.backendapi.controller;
+
+import com.example.backendapi.dominio.Filme;
+
+public class FilmeController {
+
+    public boolean isAlunoValido(Filme filme) {
+        return isNomeValido(filme) && isGeneroValido(filme) && isProtagonistaValido(filme);
+    }
+
+    private boolean isNomeValido(Filme filme) {
+        if ((filme.getNome().isEmpty()) || filme.getNome().length() < 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isGeneroValido(Filme filme) {
+        if (filme.getGenero().isEmpty())
+            return false;
+        if ((filme.getGenero().length() < 3) || filme.getGenero().length() > 20)
+            return false;
+
+        return true;
+    }
+
+    public boolean isProtagonistaValido(Filme filme) {
+        if ((filme.getProtagonista().isEmpty()) || filme.getProtagonista().length() < 1)
+            return false;
+
+        return true;
+    }
+}
