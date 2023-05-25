dd38074f59a814a054729171ea5d24f99316dcf0
Pedro Sudré <pedrosudre04@gmail.com>
2023-05-25 08:50:18 -0300

    cross origin

--- src/main/java/com/example/backendapi/BackendConfig.java
+++ src/main/java/com/example/backendapi/BackendConfig.java
@@ -2,10 +2,17 @@ package com.example.backendapi;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class BackendConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
+        registry.addMapping("/api/**") // Defina o padrão de URL que deseja permitir
+                .allowedOrigins("http://127.0.0.1:5500") // Defina a origem permitida (ou "*" para permitir todas)
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Defina os métodos HTTP permitidos
+                .allowedHeaders("*"); // Defina os cabeçalhos permitidos (ou "*" para permitir todos)
     }
 }
